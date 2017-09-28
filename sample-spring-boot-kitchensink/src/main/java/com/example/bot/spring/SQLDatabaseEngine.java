68320f3f7a49db196de82dd51f8a5484af5f9712
Chiu Tin Shing <BMDB@ChiudeMacBook-Pro.local>
2017-09-28 18:52:32 +0800

    Test #2

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -5,6 +5,9 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 @Slf4j
@@ -12,7 +15,19 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
 		//Write your code here
-		return null;
+		String resultString;
+		
+		Connection connection = getConnection();
+		PreparedStatement stmt = connection.prepareStatement("SELECT response FROM hellotable WHERE keyword="+text);
+		ResultSet rs = stmt.executeQuery();
+		while (rs.next()) {
+			resultString = rs.getString(0);
+		}
+		rs.close();
+		stmt.close();
+		connection.close();
+		
+		return resultString;
 	}
 	
 	
@@ -26,6 +41,15 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 
 		log.info("Username: {} Password: {}", username, password);
 		log.info ("dbUrl: {}", dbUrl);
+		
+//		URI dbUri = new URI(System.getenv("postgres://itniqlvmjieeeq:1bf7632721043bf02f0f498974ece3939ed3e947b9c96435ca0ced9d06593f33@ec2-107-22-235-167.compute-1.amazonaws.com:5432/daj5i9364g5dfv"));
+//
+//		String username = itniqlvmjieeeq;
+//		String password = 1bf7632721043bf02f0f498974ece3939ed3e947b9c96435ca0ced9d06593f33;
+//		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + '5432' + dbUri.getPath() +  "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+//
+//		log.info("Username: {} Password: {}", username, password);
+//		log.info ("dbUrl: {}", dbUrl);
 		
 		connection = DriverManager.getConnection(dbUrl, username, password);
 
